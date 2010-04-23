5764432d2b99330950574ef5798b517006378934
killanaca <killanaca@gmail.com>
2010-04-23 23:46:07 +0200

    add event header

---
+++ event.h
@@ -0,0 +1,31 @@
+/*
+**  File:   event.h
+**  Author: Maurizio Munafo'
+**  Description: Definizioni, strutture dati e prototipi per la gestione
+**               della lista degli eventi
+*/
+
+#include "simtime.h"
+
+typedef struct event_unit Event;
+
+/*
+  L'Event List e' una lista circolare bilinkata, referenziata dal puntatore
+  all'ultimo elemento
+*/
+struct event_unit
+      {
+	Event *next;	/* Evento successivo */
+	Event *prev;    /* Evento precedente */
+	Time  time;     /* Istante in cui deve avvenire la schedulazione
+			   dell'evento */
+	int   type;     /* Tipo di evento, da definire volta per volta */
+
+    int station;
+      };
+
+/* Prototipi */
+void insert_event(Event **, Event *);	/* Inserisce un'evento nel FES */
+Event *get_event(Event **);		/* Preleva il primo evento dal FES */
+Event *new_event(void );		/* Ritorna un nuovo evento */
+void release_event(Event *);		/* Rilascia un evento */
