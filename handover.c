2f6fa21693bdebde0d66da22d40753409b5be76f
killanaca <killanaca@gmail.com>
2010-04-24 11:19:27 +0200

    upate handover.x

---
+++ handover.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "event.h"
+#include "record.h"
+#include "simtime.h"
+#include <math.h>
+//code event
+
+//global constant
+
+
+long seme1 = 14123451,
+     seme2 =57645805;
+	 
+Time current_time;
+
+
+int main(){
+
+
+
+return 0;
+}
